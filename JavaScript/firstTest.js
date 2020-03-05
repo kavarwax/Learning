d82643c715e3eee43eb81f72d5843073fd2025d5
Max <maxime.vonthron@gmail.com>
2020-03-05 09:49:34 +0900

    Uploading local learning folders

---
+++ JavaScript/firstTest.js
@@ -0,0 +1,6 @@
+let myBook = {
+    title: 'The Story of Tau',
+    author: 'Will Alexander',
+    numberOfPages: 250,
+    isAvailable: true
+};
---
+++ Python/firsttest.py
@@ -0,0 +1,2 @@
+chaine2 = "\"Le seul individu formé, c'est celui qui a appris comment apprendre (...)\" (Karl Rogers, 1976)"
+print (chaine2)
