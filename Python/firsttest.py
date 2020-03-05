64d83f49a70aea78fff754f4f4576cc3d9599ee3
Max <maxime.vonthron@gmail.com>
2020-03-05 15:41:08 +0900

    Dev tests divers

--- Python/firsttest.py
+++ Python/firsttest.py
@@ -1,2 +1,19 @@
-chaine2 = "\"Le seul individu formé, c'est celui qui a appris comment apprendre (...)\" (Karl Rogers, 1976)"
-print (chaine2)
+# -*-coding:Latin-1 -*
+
+from math import sqrt as racine_caree
+
+def carre(valeur):
+    return valeur * valeur
+
+sqs_batch_done = False
+indent = 2
+
+#while sqs_batch_done != True:
+#    print ('valeur avant function =', indent)
+#    indent = carre(indent)
+#    print ('valeur =', indent)
+#    if indent >= 8 :
+#        sqs_batch_done = True
+#print ('End of program, valeur =', indent)
+
+print (racine_caree(16))
